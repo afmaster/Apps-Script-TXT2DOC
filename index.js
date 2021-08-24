fa25dd8215d509b75607ae0c1ada8a1c98b74054
afmaster <andre@franciscatto.com>
2021-08-24 14:17:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+function txtToDoc() {
+
+  const destinationOperados = DriveApp.getFolderById("1kEjSGZOyemkuMkztfUja9Vc_tNoBq9kY");
+
+  let files = destinationOperados.getFiles();
+
+  while (files.hasNext()) {
+    let file = files.next();
+    let fileStr = file.toString();
+    let fileNameList = fileStr.split(".");
+    let fileNameFirst = fileNameList[0];
+    let fileName = fileNameFirst.toString();
+    Logger.log(fileStr);
+
+    //let fr = new FileReader();
+    //txtText = fr.readAsText(file);
+
+    var txtText = file.getBlob().getDataAsString();
+    Logger.log(txtText);
+
+    
+
+    docName = (fileName + '.doc');
+
+    try {
+
+      var newDoc = 
+      DocumentApp.create(docName);
+      var id = newDoc.getId();
+
+    }
+
+    catch (err) {
+
+      DocumentApp.create(docName);
+
+      fileList =  DriveApp.getFilesByName(docName);
+
+      while (fileList.hasNext()) {  
+        id = fileList.next().getId();
+
+      }
+
+    }
+
+    var doc = DocumentApp.openById(id);
+    var body = doc.getBody();
+    var paragraph = body.getParagraphs()[0];
+    paragraph.insertText(0, txtText);
+    var docFile = DriveApp.getFileById(id);
+    
+    destinationOperados.addFile(docFile); 
+
+    DriveApp.getRootFolder().removeFile(docFile);
+
+    destinationOperados.removeFile(file);
+
+    }
+
+}
